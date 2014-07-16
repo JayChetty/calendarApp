10a858cdb0e235a65fdd2f7bebc093b2c9e8c326
JayChetty <jaypaulchetty@gmail.com>
2014-07-16 21:44:19 +0100

    refactor

--- client/pages/info.js
+++ client/pages/info.js
@@ -6,48 +6,49 @@ var View = require('ampersand-view');
 module.exports = View.extend({
     pageTitle: 'more info',
     template: "<div><canvas id='cycles' width='500px;' height='500px' style='border:1px solid #000000;'></canvas></div>",
+    
+    params: {
+      center: {x:250, y:250},
+      radius: 100
+    },
+
     render: function () {
         this.renderWithTemplate();
-        console.log('rendering!', $(this.el).find('#cycles'));
-        var canvas = $(this.el).find('#cycles')[0];
-        this.drawCycles(canvas);
+        this.findCanvas();
+        this.drawYearCircle();
+        this.drawMoonthLines();
         // console.log('rendering!', this.el.getElementById('cycles'))
     },
 
-    polarToCartesian: function(center, angle, length){
-      x = center.x + (length * Math.cos(angle));
-      y = center.y + (length * Math.sin(angle));
+    findCanvas: function(){
+      this.canvas = $(this.el).find('#cycles')[0];
+    },
+
+    polarToCartesian: function(angle, length){
+      x = this.params.center.x + (length * Math.cos(angle));
+      y = this.params.center.y + (length * Math.sin(angle));
       return ({x:x, y:y});
     },
 
-    drawCycles: function(canvas){
-      console.log("this", this.model.earthCycles)
-
-      center = {x: 250, y: 250}
-      console.log('drawing canvas');
-      context = canvas.getContext("2d");
-      radius = 100
-      context.arc(center.x,center.y,radius,0, 2*Math.PI);
+    drawYearCircle: function(){
+      context = this.canvas.getContext("2d");
+      context.arc(this.params.center.x,this.params.center.y,this.params.radius,0, 2*Math.PI);
       context.stroke();
+    },
 
+    drawMoonthLines: function(){
+      context = this.canvas.getContext("2d");
       moonths = this.model.earthCycles.moonths
       daysInYear = this.model.earthCycles.daysInYear()
       cumulativeDays = 0
       for (i=0;i<moonths.length;i++) {
         moonthLength = moonths[i];
         cumulativeDays = cumulativeDays + moonthLength;
-        console.log('cumdays', cumulativeDays)
-        console.log('cuASDFmdays', daysInYear)
-
-
         fractionOfYear = cumulativeDays/daysInYear;
-        console.log('frac', fractionOfYear)
         angle = (Math.PI*2) * fractionOfYear;
-        console.log('angle',angle)
         context.beginPath();
-        context.moveTo(center.x, center.y);
-        toPoint = this.polarToCartesian(center,angle,radius);
-        console.log('lineto', toPoint)
+        context.moveTo(this.params.center.x, this.params.center.y);
+        toPoint = this.polarToCartesian(angle,this.params.radius);
         context.lineTo(toPoint.x, toPoint.y);
         context.stroke();
       }
